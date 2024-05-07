27bec0936da7838d3e74f5ab0a5ce3beb21e8360
Beushawt <126538961+Beushawt@users.noreply.github.com>
2024-05-07 11:54:40 +0700

    Add files via upload

---
+++ Praktikum 7/latihanMedium/Senjata.java
@@ -0,0 +1,35 @@
+/*Nama : Sarah Teguh Kinanti Situmeang
+  NIM  : 24060122120032
+ */
+
+ package latihanMedium;
+ public class Senjata {
+     private String bunyi;
+     public int peluru;
+     private boolean menusuk;
+     
+     public Senjata(String bunyi){
+         this.bunyi = bunyi;
+         this.peluru = peluru; //this.peluru = peluru;
+     }
+     public String getBunyi(){
+         return bunyi;
+     }
+     public int getPeluru(){
+         return peluru;
+     }
+     public boolean isMenusuk(){
+         return menusuk;
+     }
+     public void setBunyi(String bunyi){
+         this.bunyi = bunyi;
+     }
+     public void setMenusuk(boolean menusuk){
+         this.menusuk = menusuk;
+     }
+     public void setPeluru(int peluru){
+         this.peluru = peluru;
+     }
+     
+ }
+ 
